a910e9b9bf660c0287328efaec5ce1ab5baf1673
psil123 <psil123@users.noreply.github.com>
2018-06-20 00:26:12 +0530

    modified:   DataGenerator.java 	modified:   ../test/Test.java 	modified:   ../../target/classes/Job/JobScheduler.class 	modified:   ../../target/classes/test/Test.class

--- JobScheduling/src/Data/DataGenerator.java
+++ JobScheduling/src/Data/DataGenerator.java
@@ -17,42 +17,42 @@ public class DataGenerator
 	/* Generating a list of jobs using Potts method
 	 * */
 	private String fname;
-	List<Job> joblist;
+	private List<Job> joblist;
 	public DataGenerator(String fname)
 	{
 		this.fname=fname;
 	}
-	public void createData(int n,double r,double t,int MAX_PI,int MIN_WI,int MAX_WI) throws Exception
+	public void manageData(int n,double r,double t,int MAX_PI,int MIN_WI,int MAX_WI) throws Exception
 	{
 		if(fname.length()==0)
 			generatePott(n,r,t,MAX_PI,MIN_WI,MAX_WI);
 		else
 			readFromCSV(fname);
 	}
-	void generatePott(int n,double r,double t,int MAX_PI,int MIN_WI,int MAX_WI) throws IOException
+	private void generatePott(int n,double r,double t,int MAX_PI,int MIN_WI,int MAX_WI) throws IOException
 	{
 		int P=0,C=0;
 		joblist=new ArrayList<Job>();
 		for(int i=0;i<n;i++)
 		{
-			joblist.add(new Job(Integer.toString(i), (int)(1+(MAX_PI-1)*Math.random()), 0, 0));
-			P+=joblist.get(i).processingTime;
+			getJoblist().add(new Job(Integer.toString(i), (int)(1+(MAX_PI-1)*Math.random()), 0, 0));
+			P+=getJoblist().get(i).processingTime;
 		}
 		for(int i=0;i<n;i++) 
 		{
-			C+=joblist.get(i).processingTime;
-			joblist.get(i).dueDate=(int)(Math.ceil(P*(1-t-r/2))+(Math.ceil(P*(1-t+r/2))-Math.ceil(P*(1-t-r/2)))*Math.random());
-			joblist.get(i).setUpTime=(int)(C/2*Math.random());
-			joblist.get(i).waitTime=(int)(MIN_WI+(MAX_WI-MIN_WI)*Math.random());
+			C+=getJoblist().get(i).processingTime;
+			getJoblist().get(i).dueDate=(int)(Math.ceil(P*(1-t-r/2))+(Math.ceil(P*(1-t+r/2))-Math.ceil(P*(1-t-r/2)))*Math.random());
+			getJoblist().get(i).setUpTime=(int)(C/2*Math.random());
+			getJoblist().get(i).waitTime=(int)(MIN_WI+(MAX_WI-MIN_WI)*Math.random());
 		}
 		writeToCSV();
 	}
 	
-	 void writeToCSV() throws IOException
+	private void writeToCSV() throws IOException
 	{
 		File file =new File("Data\\potts_n_jobs_"+new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date())+".csv");
 		FileWriter fw=new FileWriter(file);
-		for(Job i:joblist)
+		for(Job i:getJoblist())
 			fw.append(i.toString()+"\n");
 		fw.flush();
 		fw.close();
@@ -60,14 +60,17 @@ public class DataGenerator
 		
 	}
 	
-	 void readFromCSV(String fname) throws Exception
+	private void readFromCSV(String fname) throws Exception
 	{
 		
 		BufferedReader read= new BufferedReader(new FileReader("Data\\"+fname));
 		String line=null;
 		joblist=new ArrayList<Job>();
 		while((line=read.readLine())!=null)
-			joblist.add(new Job(line));
+			getJoblist().add(new Job(line));
 		read.close();
+	}
+	public List<Job> getJoblist() {
+		return joblist;
 	}
 }
